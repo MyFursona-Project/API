264e5f1b4eec102ddc1bc36e447af43ef3d40329
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-12-29 21:26:06 -0600

    Added Login

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,22 @@ const prisma = new PrismaClient();
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver, ProfileResolver } from "./resolvers/";
+import * as dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 const main = async () => {
   const app = express();
+  app.use(cookieParser());
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, ProfileResolver],
       validate: false,
     }),
+    context: ({ req, res }) => ({ req, res }),
   });
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app,  });
+  apolloServer.applyMiddleware({ app });
   app.use("/", routes);
   app.listen(8080, () => {
     console.log("MyFursona is Listening in port 8080");
@@ -33,6 +38,4 @@ main()
     process.exit(1);
   });
 
-  export {
-    prisma
-  }
+export { prisma };
